5117dda09f84d5ed4b93deb592388697ab9a9a2d
skonieczny08 <skoniecznythamily@gmail.com>
2024-11-19 13:39:08 +0000

    atualização da calculadora

--- src/index.mjs
+++ src/index.mjs
@@ -19,6 +19,9 @@ function calcular() {
   subtracao.innerHTML = `A subtracao de ${n1} e ${n2} é: ${n1 - n2}`;
   multiplicacao.innerHTML = `A multiplicacao de ${n1} e ${n2} é: ${n1 * n2}`;
   divisao.innerHTML = `A divisao de ${n1} e ${n2} é: ${n1 / n2}`;
+  //MANIPULANDO DOM COM JAVACRIPT
+  resultados.classList.add("mostrar");
+  resultados.classList.remove("esconder");
 
   //return res
 }
